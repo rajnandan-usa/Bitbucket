f902d10dbff34a95016e92ecd496591f467a9a6f
Rajnandan Kushwaha <rajnandanusa@gmail.com>
2023-07-30 23:54:52 +0530

    Update admin-ajax.php

--- admin-ajax.php
+++ admin-ajax.php
@@ -1,6 +1,10 @@
 <?php
 require_once('../../../wp-load.php');
 
+add_action('wp_ajax_contact_management_save_contact', 'contact_management_save_contact');
+add_action('wp_ajax_nopriv_contact_management_save_contact', 'contact_management_save_contact');
+
+
 
 function contact_management_save_contact()
 {
@@ -12,12 +16,17 @@ function contact_management_save_contact()
     wp_die();
 }
 
-function contact_management_delete_contact()
-{
-    if (class_exists('Contact_Management_Plugin')) {
-        $contact_management_plugin = new Contact_Management_Plugin();
-        $contact_management_plugin->delete_contact();
-    }
+// function contact_management_delete_contact()
+// {
+    
+//     if (class_exists('Contact_Management_Plugin')) {
+//         $contact_management_plugin = new Contact_Management_Plugin();
+//         $contact_management_plugin->delete_contact();
+//     }
 
-    wp_die();
-}
+//     wp_die();
+// }
+
+
+
+?>
